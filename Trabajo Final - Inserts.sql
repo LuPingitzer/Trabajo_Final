0aae183a74e66fcf11793c43e4905ff60b73f72e
Nicr0ck <nicolas.sagues25@gmail.com>
2024-10-25 17:45:18 -0300

    Agregada BD
    BDrawrrr

---
+++ Trabajo Final - Inserts.sql
@@ -0,0 +1,5 @@
+INSERT INTO productos (nombre, descripcion, precio, cantidad) values (
+"Botones Rojos","Rallados Grandes de 3 agujeros", 2000,40);
+INSERT INTO ventas(id_usuario) values (1);
+INSERT INTO detalles_ventas(id_venta, id_producto, cantidad) values
+(1,1, 30);
